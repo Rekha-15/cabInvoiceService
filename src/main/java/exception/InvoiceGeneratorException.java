53cec2638f1b2c78e32302695313916f8e8a9788
sanobar28 <sanobar.mujawar@gmail.com>
2021-07-01 18:07:08 +0530

    refactor: indentation

--- src/main/java/exception/InvoiceGeneratorException.java
+++ src/main/java/exception/InvoiceGeneratorException.java
@@ -1,19 +1,18 @@
 package exception;
 
 public class InvoiceGeneratorException extends Exception {
-	
+
 	private static final long serialVersionUID = 1L;
 
-	public enum ExceptionType
-    {
-        USER_ALREADY_EXISTS
-    }
-    public ExceptionType type;
+	public enum ExceptionType {
+		USER_ALREADY_EXISTS
+	}
 
-    public InvoiceGeneratorException(ExceptionType type, String message)
-    {
-        super(message);
-        this.type = type;
-    }
+	public ExceptionType type;
+
+	public InvoiceGeneratorException(ExceptionType type, String message) {
+		super(message);
+		this.type = type;
+	}
 
 }
