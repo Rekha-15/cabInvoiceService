e9bcfd82157a520e35dc95660b3ef50c012c7455
Rekha-15 <rekhapatil.1509@gmail.com>
2021-07-01 15:05:27 -0400

    added :  Calculate Fare : .

--- src/test/java/com/InvoiceService/InvoiceServiceTest.java
+++ src/test/java/com/InvoiceService/InvoiceServiceTest.java
@@ -11,9 +11,6 @@ public class InvoiceServiceTest {
 		InvoiceGenerator cabInvoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
-     // giving distance and time
-     		//25 rs should be cost, so assigning local variable to this
-     		//that is fare = LV
         double totalFare = cabInvoiceGenerator.calculateFare(distance,time);
         System.out.println("Total Fare = " +totalFare);
         Assert.assertEquals(25, totalFare,0.0);
